8a707b9a2abbb0b2e8782130cdc2fe7887788a48
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2023-12-29 07:23:56 +0530

    Contains maximum mathematical operations

---
+++ Simple calculator.py
@@ -0,0 +1,14 @@
+print('This is a simple calculator.')
+print('It can do some simple calculations like addition, substraction, multiplication and division')
+print('Please enter the first number')
+num1= float(input())
+print('Now please enter the second number')
+num2= float(input())
+mul= num1*num2
+div= num1/num2
+add= num1+num2
+sub= num1-num2
+print('If u add these numbers '+add+" this will be the answer")
+print('If u substract the numbers '+sub+" this will be the answer")
+print("If u multiply the numbers "+mul+' this will be the answer')
+print('If u divide the numbers '+div+' this will be the answer')
