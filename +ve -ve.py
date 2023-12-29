a46717f4c654a1afc898961faad731ddbd92dfbd
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2023-12-29 07:27:43 +0530

    To check if the number is +ve or -ve

---
+++ +ve -ve.py
@@ -0,0 +1,7 @@
+n = int(input('Type any number: '))
+if n > 0:
+    print('The number u inputed just now is a positive integer.')
+elif n == 0:
+    print('It is zero')
+else:
+    print('The number you just inputed is negative')
