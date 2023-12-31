89d0ff906cb6d89e465e1da2a292da9c4864b47d
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2023-12-31 06:57:16 +0530

    Finding fibonacci number series

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+def fibo (n):
+    if n <= 1:
+        return n
+    else:
+        return fibo (n-1) + fibo (n-2)
+n = int(input('Enter the size of the series: '))
+if n <= 0:
+    print('Enter a positive number')
+else:
+    print('Fibonacci Series:')
+    for i in range (n):
+        print (fibo(i))
