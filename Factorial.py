74a0db3a4908403d3196885c0fa706edb2ba5f91
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2024-01-02 15:56:51 +0530

    Finding Factorial of a number

--- Factorial.py
+++ Factorial.py
@@ -1,5 +1,15 @@
-i = int(input ( 'here: '))
-while i > 2:
-    k = i - 1
-    print (k*i)
-    i = i - 1
+num = int(input('Enter the number here: '))
+
+k = 1
+
+if num == 0:
+    print('The factorial of 0 is 1.')
+    
+elif num < 0:
+    print ("Factorial of negetive numbers doesn't exist.")
+
+else:
+    for i in range (1, num+1):
+        k= k *i
+
+print(k)
