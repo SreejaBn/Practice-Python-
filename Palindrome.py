575e5db71e8538ecda95b0cc7449782b5f7b4589
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2024-01-19 21:12:50 +0530

    Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -2,7 +2,7 @@ print('\nthis program will check whether the number is palindrome or not.\n')
 print('btw palindrome is number which when reversed gives the same number\n')
 print('Eg: 12321\n')
 m = int(input('Write your number here: '))
-n = int(str(m) [::-2])  #[::-1] is used to reverse the number 
+n = int(str(m) [::-1])  #[::-1] is used to reverse the number 
 if n == m:
     print ('This number is palindrome.')
 else:
