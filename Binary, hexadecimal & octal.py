ab67040e515e3ef72f13ff516000b892ab03cb45
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2024-03-13 18:49:42 +0530

    Binary, hexadecimal & octal.py

--- Binary, hexadecimal & octal.py
+++ Binary, hexadecimal & octal.py
@@ -2,3 +2,4 @@ dec = int(input('Enter your number here: '))
 print(bin(dec), "in binary.")
 print (hex(dec), "in hexadecimal.")
 print (oct(dec), "in octal.")
+print ( '%0' % dec, "in octal.") # this will print the answer without any prefix 
