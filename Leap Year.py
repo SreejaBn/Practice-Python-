a10944f8fb424509804a41657c5af971b8c86dec
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2024-03-24 20:00:32 +0530

    Checks if the year is leap or not.py

--- Leap Year.py
+++ Leap Year.py
@@ -5,3 +5,9 @@ if y % 4 == 0:
     if y % 100 == 0:
         if y % 400 == 0:
             print ("It's a leap year")
+        else:
+             print ("It's not a leap year." )
+    else:
+        print ("It's not a leap year." )
+else:
+    print ("It's not a leap year." )
