22d997af912b9939e22c722c367565cbbb061443
Sreeja Banerjee <155034501+SreejaBn@users.noreply.github.com>
2025-12-30 23:01:37 +0530

    Create class.py

---
+++ OOPs/class.py
@@ -0,0 +1,12 @@
+class Computer:
+    def __init__(self, ram, cpu):
+        self.d= ram
+        self.f= cpu
+        self.ram= 16
+        self.cpu= 'door'
+
+    def config(yo):
+        print(yo.ram, yo.cpu, yo.d, yo.f)
+
+com1= Computer(4, 'rf')
+com1.config()
